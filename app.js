09fe931486a8b5acf5c56803b17d0c9f57fc5a40
Xudiyef21 <xeyalxeyal068@gmail.com>
2023-08-26 09:59:52 +0400

    Thunder Client Added Data

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.set('view engine', 'ejs')
 
 // Static files
 app.use(express.static('public'))
+app.use(express.json())
 
 //
 app.use('/', pageRoute)
--- routes/photoRoute.js
+++ routes/photoRoute.js
@@ -1,9 +1,9 @@
-import  express from "express";
+import express from "express";
 import * as photoController from '../controllers/photoController.js'
 
 const router = express.Router()
 
-router.route('/').post(photoController.createPhoto) 
+router.route('/').post(photoController.createPhoto).get(photoController.getAllPhotos)
 
 // router.route('/about').get(pageController.getAboutPage) 
 
